36f7fcb91b2c178c82878f7d7b2ac34cb749c0ce
v-georgakopoulos <v.georgakopoulos.dev@outlook.com>
2025-11-06 19:23:24 +0200

    Finished UI styling and started implementing app logic with JavaScript

---
+++ public/script.js
@@ -0,0 +1,267 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Search ------------ //
+    const searchHistory = document.getElementById('searchHistory');
+    const searchInput = document.getElementById('searchInput');
+    const searchButton = document.getElementById('searchButton');
+    const resetButton = document.getElementById('resetButton');
+    const responseContainer = document.getElementById('response');
+
+    // Reset search history
+    function resetHistory() {
+        searchHistory.innerText = '';
+        const option = document.createElement('option');
+        option.value = '';
+        option.textContent = 'Select a previous Search';
+        searchHistory.appendChild(option);
+    }
+
+
+    // Load our search history from local storage
+    function loadSearchHistory() {
+        const savedSearches = JSON.parse(localStorage.getItem('searchHistory')) || [];
+        resetHistory();
+        savedSearches.forEach(searchTerm => {
+            const option = document.createElement('option');
+            option.value = searchTerm;
+            option.textContent = searchTerm;
+            searchHistory.appendChild(option);          
+        });
+    }
+
+    // Save the search history to localstorage
+    function saveSearchHistory(searchTerm) {
+        let savedSearches = JSON.parse(localStorage.getItem('searchHistory')) || [];
+        if(!savedSearches.includes(searchTerm)) {
+            savedSearches.push(searchTerm);
+            localStorage.setItem('searchHistory', JSON.stringify(savedSearches));
+        }
+    }
+
+    // Event listener for dropdown change
+    searchHistory.addEventListener('change', () => {
+        const selectedSearch = searchHistory.value;
+        if(selectedSearch) {
+            searchInput.value = selectedSearch;
+            searchPodcast();
+        }
+    });
+
+    // Event Listener for search button, input
+    searchButton.addEventListener('click',searchPodcast);
+    searchInput.addEventListener('keypress', (e) => {
+        if(e.key === 'Enter') {
+            searchPodcast();
+        }
+    });
+
+    // Event listener to reset search input
+    searchInput.addEventListener('focus', () => {
+        searchInput.value = '';
+    })
+
+    // Event listener for reset button
+    resetButton.addEventListener('click', () => {
+        localStorage.removeItem('searchHistory');
+        resetHistory();
+        searchInput.value = '';
+    });
+
+    // Load search history when the page load
+    loadSearchHistory();
+    
+    // Search Podcasts
+     async function searchPodcast() {
+        const searchTerm = searchInput.value.trim();
+        if(searchTerm) {
+            console.log('Searced:', searchTerm);
+            saveSearchHistory(searchTerm);
+            loadSearchHistory();
+        } else {
+            responseContainer.innerText = 'Please enter a podcast title.'
+        }
+
+        try {
+            const response = await fetch(`/api/search/?q=${encodeURIComponent(searchTerm)}`);
+            const data = await response.json();
+
+            responseContainer.textContent = '';
+            
+            if(data.feeds && data.feeds.length > 0) {
+                console.log('results:',data.feeds);
+            }else {
+                responseContainer.innerText = `No Results Found`;
+            }
+        } catch (error) {
+            responseContainer.innerText = `Error: ${error.message}`;
+        }
+    }
+
+    // Navigation ------------ //
+    const searchLink = document.getElementById('searchLink');
+    const listenLink = document.getElementById('listenLink');
+    const searchContainer = document.querySelector('.search-container');
+    const mainContainer = document.querySelector('.main-container');
+    const playerContainer = document.querySelector('.player-container');
+    const queueContainer = document.querySelector('.queue');
+    
+    searchLink.addEventListener('click',navigateToSearch);
+    listenLink.addEventListener('click',navigateToPlayer);
+    
+    function navigateToSearch() {
+        searchContainer.style.display = 'flex';
+        mainContainer.style.display = 'flex';
+        playerContainer.style.display = 'none';
+        queueContainer.style.display = 'none';
+        searchLink.classList.add('selected');
+        listenLink.classList.remove('selected')
+    }
+    
+    function navigateToPlayer() {
+        searchContainer.style.display = 'none';
+        mainContainer.style.display = 'none';
+        playerContainer.style.display = 'flex';
+        queueContainer.style.display = 'flex';
+        searchLink.classList.remove('selected');
+        listenLink.classList.add('selected')
+    }
+});
+
+
+
+
+
+
+// const image = document.querySelector('img');
+// const title = document.getElementById('title');
+// const artist = document.getElementById('artist');
+// const music = document.querySelector('audio');
+// const currentTimeEl = document.getElementById('current-time');
+// const durationEl = document.getElementById('duration');
+// const progress = document.getElementById('progress');
+// const progressContainer = document.getElementById('progress-container');
+// const prevBtn = document.getElementById('prev');
+// const playBtn = document.getElementById('play');
+// const nextBtn = document.getElementById('next');
+
+// // Music
+// const songs = [
+//   {
+//     name: 'jacinto-1',
+//     displayName: 'Electric Chill Machine',
+//     artist: 'Jacinto Design',
+//   },
+//   {
+//     name: 'jacinto-2',
+//     displayName: 'Seven Nation Army (Remix)',
+//     artist: 'Jacinto Design',
+//   },
+//   {
+//     name: 'jacinto-3',
+//     displayName: 'Goodnight, Disco Queen',
+//     artist: 'Jacinto Design',
+//   },
+//   {
+//     name: 'metric-1',
+//     displayName: 'Front Row (Remix)',
+//     artist: 'Metric/Jacinto Design',
+//   },
+// ];
+
+// // Check if Playing
+// let isPlaying = false;
+
+// // Play
+// function playSong() {
+//   isPlaying = true;
+//   playBtn.classList.replace('fa-play', 'fa-pause');
+//   playBtn.setAttribute('title', 'Pause');
+//   music.play();
+// }
+
+// // Pause
+// function pauseSong() {
+//   isPlaying = false;
+//   playBtn.classList.replace('fa-pause', 'fa-play');
+//   playBtn.setAttribute('title', 'Play');
+//   music.pause();
+// }
+
+// // Play or Pause Event Listener
+// playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+// // Update DOM
+// function loadSong(song) {
+//   title.textContent = song.displayName;
+//   artist.textContent = song.artist;
+//   music.src = `music/${song.name}.mp3`;
+//   image.src = `img/${song.name}.jpg`;
+// }
+
+// // Current Song
+// let songIndex = 0;
+
+// // Previous Song
+// function prevSong() {
+//   songIndex--;
+//   if (songIndex < 0) {
+//     songIndex = songs.length - 1;
+//   }
+//   loadSong(songs[songIndex]);
+//   playSong();
+// }
+
+// // Next Song
+// function nextSong() {
+//   songIndex++;
+//   if (songIndex > songs.length - 1) {
+//     songIndex = 0;
+//   }
+//   loadSong(songs[songIndex]);
+//   playSong();
+// }
+
+// // On Load - Select First Song
+// loadSong(songs[songIndex]);
+
+// // Update Progress Bar & Time
+// function updateProgressBar(e) {
+//   if (isPlaying) {
+//     const { duration, currentTime } = e.srcElement;
+//     // Update progress bar width
+//     const progressPercent = (currentTime / duration) * 100;
+//     progress.style.width = `${progressPercent}%`;
+//     // Calculate display for duration
+//     const durationMinutes = Math.floor(duration / 60);
+//     let durationSeconds = Math.floor(duration % 60);
+//     if (durationSeconds < 10) {
+//       durationSeconds = `0${durationSeconds}`;
+//     }
+//     // Delay switching duration Element to avoid NaN
+//     if (durationSeconds) {
+//       durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+//     }
+//     // Calculate display for currentTime
+//     const currentMinutes = Math.floor(currentTime / 60);
+//     let currentSeconds = Math.floor(currentTime % 60);
+//     if (currentSeconds < 10) {
+//       currentSeconds = `0${currentSeconds}`;
+//     }
+//     currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
+//   }
+// }
+
+// // Set Progress Bar
+// function setProgressBar(e) {
+//   const width = this.clientWidth;
+//   const clickX = e.offsetX;
+//   const { duration } = music;
+//   music.currentTime = (clickX / width) * duration;
+// }
+
+// // Event Listeners
+// prevBtn.addEventListener('click', prevSong);
+// nextBtn.addEventListener('click', nextSong);
+// music.addEventListener('ended', nextSong);
+// music.addEventListener('timeupdate', updateProgressBar);
+// progressContainer.addEventListener('click', setProgressBar);
---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+const CryptoJs = require('crypto-js');
+const fetch = require('node-fetch');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const authKey = process.env.AUTH_KEY;
+const secretKey = process.env.SECRET_KEY;
+const userAgent = process.env.USER_AGENT;
+const apiEndpoint = process.env.API_ENDPOINT;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Shared authenticaation function
+function generateAuthHeaders() {
+    const apiHeaderTime = Math.floor(new Date().getTime() / 1000);
+    const hash = CryptoJs.SHA1(authKey + secretKey + apiHeaderTime).toString(CryptoJs.enc.Hex);
+
+    return {
+        'User-Agent': userAgent,
+        'X-Auth-Key' : authKey,
+        'X-Auth-Date' : apiHeaderTime.toString(),
+        'Authorization' : hash
+    }
+}
+
+// Search for podcasts
+app.get('/api/search', async (req,res) => {
+    const query = req.query.q;
+    if(!query) {
+        return res.status(400).json({error: 'Query Parameter is required'})
+    }
+    const headers = generateAuthHeaders();
+
+    try {
+        const response = await fetch(`${apiEndpoint}/search/byterm?q=${encodeURIComponent(query)}`, {
+            method: 'get',
+            headers: headers
+        });
+        if(response.ok && response.headers.get('content-type').includes('application/json')) {
+            const data = await response.json();
+            res.json(data);
+        } else {
+            const rawText = await response.text();
+            console.log('Raw Response',rawText);
+            res.status(500).json({ error: 'Invalid response from API',rawText})
+        }
+    } catch (error) {
+        console.error('Error fetching API:' ,error.message);
+        res.status(500).json({error: error.message});
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on http://localhost:${PORT}`);
+});
